1cabb227a7e172d2243ed8ed26b1694235af4ea4
Sathesh21 <satheshmgn21@gmail.com>
2021-11-27 02:36:19 +0530

    Change directories

---
+++ Basic-mini -projects/smile/sample.js
@@ -0,0 +1,9 @@
+
+alert("are you sure");
+function ChangeContant() {
+    document.getElementById("sample").innerHTML = "paragraph modified by JS";
+}
+function sademoji() {
+    document.getElementById("smile").src = "sadsmile.png";
+}
+
--- practice-problems-in-js/sum-of-n-numbers-with-for-loop.js
+++ practice-problems-in-js/sum-of-n-numbers-with-for-loop.js
@@ -1,7 +1,7 @@
-var n = 6;
+var n = 7;
 let op = 0;
 
 for (let i = 0; i <= n; i++) {
   op = op + i;
 }
-console.log("sum of n number is", op);
+console.log(`sum of "${n}" number is `, op);
