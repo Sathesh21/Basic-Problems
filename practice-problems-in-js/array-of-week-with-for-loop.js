2abac58c73e3aaf58230523dc895fe5b1fbb1f73
Sathesh21 <satheshmgn21@gmail.com>
2021-12-20 23:12:14 +0530

    initial comit

---
+++ practice-problems-in-js/array-of-week-with-for-loop.js
@@ -0,0 +1,9 @@
+
+// array declaration with for loop
+
+let a = [1, 2, 3, 4, 5, 6, 7];
+let b = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+for(let i=0;i<a.length;i++)
+{
+      console.log(a[i], b[i]);
+}
